81bd71c89464c58527dd129a6bc2567f7c19f5c4
ZARIFMAC <uzbzarif@gmail.com>
2022-12-05 10:02:56 +0500

    weekday

--- get_post_weekday.py
+++ get_post_weekday.py
@@ -1,3 +1,4 @@
+import datetime
 from read_data import fromJson
 def get_post_weekday(data:dict)->dict:
     """
@@ -6,11 +7,30 @@ def get_post_weekday(data:dict)->dict:
         data: a dictionary of posts
     returns: a dictionary with the number of posts for each weekday
     """
-    return
+    weekdays = {
+        "Monday": 0,
+        "Tuesday": 0,
+        "Wednesday": 0,
+        "Thursday": 0,
+        "Friday": 0,
+        "Saturday": 0,
+        "Sunday": 0
+    }
+    messages = data['messages']
+    for message in messages:
+        date = message['date']
+        weekday = datetime.datetime.strptime(date,'%Y-%m-%dT%H:%M:%S').strftime('%A')
+        # Increment the weekday
+        weekdays[weekday] += 1
+        
+       
+        
+
+    return weekdays
 
 
 # Example
-data = fromJson('posts.json')
+data = fromJson('data/result.json')
 print(get_post_weekday(data))
 # output =  {
 #     'Monday': 5, 
